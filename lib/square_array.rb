6d81a4a653321c829ec4cea255ce9f400c88068c
exuberant-pool-9484 <alisherfayzimatov@gmail.com>
2019-11-11 15:46:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   new_squared_array = []
   while counter<array.length do
-  new_squared_array<<(array[counter] **2)
+  new_squared_array.push(array[counter] **2)
   counter -= -1
 end
 new_squared_array
